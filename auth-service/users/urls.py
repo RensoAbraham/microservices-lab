47623819a90a821a50ae912cfd2c4e8eeb130c72
Renso Abraham <alonsno138@gmail.com>
2025-10-27 09:32:48 -0500

    Reorganizando el proyecto en ramas, Ejercicio1 (Inicial)

--- auth-service/users/urls.py
+++
@@ -1,8 +0,0 @@
-from djoser import views as djoser_views
-from django.urls import path
-from .views import RegisterView
-
-urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('me/', djoser_views.UserViewSet.as_view({'get': 'me'}), name='user-me'),
-]
